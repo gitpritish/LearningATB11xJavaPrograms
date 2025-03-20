eba0d9ab1f6639bb11dc67b42709b90a03e533b9
pritish <pritish.k@loginextsolutions.com>
2025-03-20 09:58:39 +0530

    20th March new repository created and learning the intellij and GIT

---
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        //System.out.println("Learning the comments");
+        System.out.println("You learned how to comment out code");
+        /*int a = 10;
+        int b = 20;*/
+        System.out.println("Now you learned how to comment the multiple lines");
+    }
+}
