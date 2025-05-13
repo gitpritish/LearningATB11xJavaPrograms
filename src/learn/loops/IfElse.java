038a33ef11050b07c6501e739e7d95c637a78f61
pritish <pritish.k@loginextsolutions.com>
2025-05-13 23:27:53 +0530

    Moved and added loop concept Java files

---
+++ src/learn/loops/IfElse.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class IfElse {
+
+	public IfElse() {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("the comeback");
+		// TODO Auto-generated method stub
+
+	}
+}
